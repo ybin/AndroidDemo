366b2056a025d7b4b63f6fb83fc09cfd6e3d7617
ybin sun <ybin.sun@gmail.com>
2014-03-20 13:28:25 +0800

    DefineView demo: break line automatically. Ignore 'newline' char.

--- DefineView/src/com/example/defineview/CustomView.java
+++ DefineView/src/com/example/defineview/CustomView.java
@@ -49,7 +49,10 @@ public class CustomView extends View {
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		setMeasuredDimension(measureWidth(widthMeasureSpec), measureHeight(heightMeasureSpec));
+		int width = measureWidth(widthMeasureSpec);
+		int height = measureHeight(heightMeasureSpec, width - getPaddingLeft() - getPaddingRight());
+		
+		setMeasuredDimension(width, height);
 	}
 
 	@Override
@@ -59,7 +62,26 @@ public class CustomView extends View {
 			mBackground.draw(canvas);
 		}
 		// 字体的纵向原点是其baseline，而不是左上角
-		canvas.drawText(mText, 0, -mPaint.ascent(), mPaint);
+		// 不考虑自动换行的情况
+		//canvas.drawText(mText, 0, -mPaint.ascent(), mPaint);
+		
+		// 考虑自动换行的情况
+		drawText(canvas);
+	}
+	
+	// 一行一行的绘制文本，注意：没有考虑文本中存在换行符的情况
+	private void drawText(Canvas canvas) {
+		int start = 0;
+		int end;
+		float lineHeight = mPaint.descent() - mPaint.ascent();
+		int lineWidth = getWidth() - getPaddingLeft() - getPaddingRight();
+		float y = getPaddingTop() - mPaint.ascent();
+		
+		while((end = findEndIndex(start, lineWidth)) != -1) {
+			canvas.drawText(mText, start, end, 0, y, mPaint);
+			start = end;
+			y += lineHeight;
+		}
 	}
 
 	private int measureWidth(int spec) {
@@ -84,25 +106,64 @@ public class CustomView extends View {
 		return result;
 	}
 
-	private int measureHeight(int spec) {
-		int result = 0;
+	private int measureHeight(int spec, int lineWidth) {
+		int fontHeight = (int)(mPaint.descent() - mPaint.ascent());
+		int lineCount = countLines((int)mPaint.measureText(mText), lineWidth);
+		
+		int contentHeight = 0;
 		int mode = MeasureSpec.getMode(spec);
 		int size = MeasureSpec.getSize(spec);
 
 		switch (mode) {
 		case MeasureSpec.EXACTLY: {
-			result = size;
+			contentHeight = size;
 			break;
 		}
 		case MeasureSpec.UNSPECIFIED:
 		case MeasureSpec.AT_MOST: {
-			result = (int) ((mPaint.descent() - mPaint.ascent()) + getPaddingTop() + getPaddingBottom());
+			contentHeight = fontHeight * lineCount + getPaddingTop() + getPaddingBottom();
 			if (mode == MeasureSpec.AT_MOST) {
-				result = Math.min(result, size);
+				contentHeight = Math.min(contentHeight, size);
 			}
 			break;
 		}
 		}
-		return result;
+		return contentHeight;
+	}
+	
+	private int countLines(int contentWidth, int lineWidth) {
+		int count = 0;
+		
+		// 这种计算方法会存在误差，因为没有考虑行尾的剩余空间无法放下一个字符的情况
+		//count = (contentWidth + lineWidth - 1) / lineWidth;
+		
+		// 考虑到行尾的剩余空间无法放下一个字符的情况
+		int start = 0;
+		int end;
+		while((end = findEndIndex(start, lineWidth)) != -1) {
+			count += 1;
+			start = end;
+		}
+		return count == 0 ? 1 : count;
+	}
+	
+	/*
+	 *  这种方法效率比较低，需要优化，比如可以传入上一行的字符个数
+	 *  作为参考值，然后end的值根据实际情况增加或者减少。
+	 */
+	private int findEndIndex(int start, int lineWidth) {
+		int end = start;
+		int textLength = mText.length();
+		
+		while(end < textLength 
+				&& (int)mPaint.measureText(mText, start, end + 1) <= lineWidth) {
+			end += 1;
+		}
+		
+		if(end == start) {
+			return -1;
+		}
+		
+		return end;
 	}
 }
